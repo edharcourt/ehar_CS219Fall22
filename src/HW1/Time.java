c771edd2edd57a5fb29a8ac53b55f01dbf4c1cd4
Ed Harcourt <edharcourt@gmail.com>
2022-09-01 10:50:07 -0400

    Uploading HW 1

---
+++ src/HW1/Time.java
@@ -0,0 +1,21 @@
+package HW1;
+
+public class Time {
+    public static void main(String [] args) {
+        int hour = 16;
+        int minute = 43;
+        int sec = 25;
+
+        // Seconds since midnight
+        int ssm = hour * 60 * 60 + minute * 60 + sec;
+
+        System.out.println("Homework 1");
+        System.out.println(hour + ":" + minute + ":" + sec + " is " +
+                ssm + " seconds since midnight");
+
+        // Seconds in a day
+        double sid = 24*60*60.0;
+
+        System.out.println(Math.round(ssm/sid*100) + "% of the day has elapsed");
+    }
+}
