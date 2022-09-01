6b67d5aa48ea492d3cbe2f3be9aa54450cc30742
ehar <edharcourt@gmail.com>
2022-09-01 15:47:37 -0400

    Use packages and new examples.

---
+++ src/HW2/DayOfWeek.java
@@ -0,0 +1,4 @@
+package HW2;
+
+public class DayOfWeek {
+}
---
+++ src/Utility/Weather.java
@@ -0,0 +1,29 @@
+package Utility;
+
+import java.util.Scanner;
+
+public class Weather {
+
+    public static double windchill(double t,    // temperature
+                                   double v) {  // velocity in MPH
+        return 35.75 + 0.6215*t + (0.4275*t - 35.75)*Math.pow(v, 0.16);
+    }
+
+    public static void main(String [] args) {
+
+        // Used for getting input from the keyboard
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.print("Enter temperature in degrees F: ");
+        double temperature = kbd.nextDouble();
+
+        System.out.print("Enter wind velocity in MPH: ");
+        double velocity = kbd.nextDouble();
+
+        System.out.printf("The windchill for %.1f degrees at %.1f MPH is %.2f\n",
+                temperature, velocity, windchill(temperature, velocity));
+
+        System.out.println("They said \"Don't\ndo\nit\"");
+    }
+
+}
