6f10f482785bb082def6d4b3f8f237c06771fa75
ehar <edharcourt@gmail.com>
2022-10-27 15:58:10 -0400

    See RecursionExamples.Recursion for what we did in class on 10/27/22.

--- src/RecursionExamples/Recursion.java
+++ src/RecursionExamples/Recursion.java
@@ -5,28 +5,46 @@ public class Recursion {
     // sum of the digits
     // sod(1234) ==  10
     public static int sod(int n) {
-        return -1;  // shut up error message
+        if (n == 0)
+            return 0;
+        else
+            return sod(n/10) + n % 10;
     }
 
     // count7s(7132771) == 3
     public static int count7s(int n) {
-        return -1;   // shut up error message
+        if (n == 0)
+            return 0;
+        else if (n % 10 == 7)
+            return count7s(n / 10) + 1;
+        else
+            return count7s(n / 10);
     }
 
     // pow(2, 10) == 1024
     public static int pow(int x, int y) {
-        return -1;// shut up error message
+        if (y == 0)
+            return 1;
+        else
+            return x * pow(x, y-1);
     }
 
     // reverse("hello").equals("olleh")
     public static String reverse(String s) {
-        return null;// shut up error message
+        if (s.length() == 0)
+            return "";
+        else
+            return reverse(s.substring(1)) + s.charAt(0);
     }
 
     // removeSpaces("  h   e ll    o  ").equals("hello")
     public static String removeSpaces(String s) {
-        return null;  // shut up error message
-        // change it to remove punctuation and spaces?
+        if (s.length() == 0)
+            return "";
+        else if (s.charAt(0) == ' ')
+            return removeSpaces(s.substring(1));
+        else
+            return s.charAt(0) + removeSpaces(s.substring(1));
     }
 
     // Don't write recursively, use previous functions
@@ -35,13 +53,20 @@ public class Recursion {
     // racecar, repaper, rotator, detartrated
     // saippuakivikauppias (Finnish for a dealer in lye)
     public static boolean isPalindrome(String s) {
-        return false;// shut up error message
+        s = removeSpaces(s);
+        return s.equals(reverse(s));// shut up error message
     }
 
     // write recursively not using functions we wrote. Just string
     // function CharAt
     public static boolean isPalindrome2(String s) {
-        return true;
+        if (s.length() < 2)
+            return true;
+        else if (s.charAt(0) == s.charAt(s.length() - 1) &&
+                 isPalindrome2(s.substring(1,s.length() - 1)))
+            return true;
+        else
+            return false;
     }
 
     // printBinary(23) should print 010111.
@@ -55,6 +80,8 @@ public class Recursion {
     }
 
     // sum(new int []{1,2,3,4}) == 10
+    // Do for exam study
+    // i is the current position in the array
     public static int sum(int[] a, int i) {
         return -1;  // shut up error message
     }
@@ -68,13 +95,23 @@ public class Recursion {
 
     // Arrays.equals(new int [] {1,2,3,4},
     //               new int [] {4,3,2,1})
+    // Do for exam study
+    // i and j are the left and right bounds of the array
     public static void reverse(int [] a, int i, int j) {
 
     }
 
     // main
     public static void main(String[] args) {
-
+        System.out.println(sod(4192) == 16);
+        System.out.println(count7s(7142771) == 3);
+        System.out.println(pow(2,10) == 1024);
+        System.out.println(reverse("hello").equals("olleh"));
+        System.out.println((isPalindrome("go hang a salami im a lasagna hog")));
+        System.out.println(removeSpaces(
+                "go hang a salami im a lasagna hog").
+                equals("gohangasalamiimalasagnahog"));
+        System.out.println(isPalindrome2("racecar"));
     }
 
 }  // class Recursion
