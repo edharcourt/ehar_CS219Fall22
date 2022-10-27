521274b704bf44a5c77ce02f6606c51a93e75c92
Ed Harcourt <ehar@stlawu.edu>
2022-10-26 20:35:28 -0400

    Add RecursionExamples.Recursion for class on 10/27/22.

---
+++ src/RecursionExamples/Recursion.java
@@ -0,0 +1,80 @@
+package RecursionExamples;
+
+public class Recursion {
+
+    // sum of the digits
+    // sod(1234) ==  10
+    public static int sod(int n) {
+        return -1;  // shut up error message
+    }
+
+    // count7s(7132771) == 3
+    public static int count7s(int n) {
+        return -1;   // shut up error message
+    }
+
+    // pow(2, 10) == 1024
+    public static int pow(int x, int y) {
+        return -1;// shut up error message
+    }
+
+    // reverse("hello").equals("olleh")
+    public static String reverse(String s) {
+        return null;// shut up error message
+    }
+
+    // removeSpaces("  h   e ll    o  ").equals("hello")
+    public static String removeSpaces(String s) {
+        return null;  // shut up error message
+        // change it to remove punctuation and spaces?
+    }
+
+    // Don't write recursively, use previous functions
+    // Go hang a salami I'm a lasagna hog
+    // Eva, can I see bees in a cave?
+    // racecar, repaper, rotator, detartrated
+    // saippuakivikauppias (Finnish for a dealer in lye)
+    public static boolean isPalindrome(String s) {
+        return false;// shut up error message
+    }
+
+    // write recursively not using functions we wrote. Just string
+    // function CharAt
+    public static boolean isPalindrome2(String s) {
+        return true;
+    }
+
+    // printBinary(23) should print 010111.
+    public static void printBinary(int n) {
+
+    }
+
+    // toBinary(23).equals("010111")
+    public static String toBinary(int n) {
+        return null;
+    }
+
+    // sum(new int []{1,2,3,4}) == 10
+    public static int sum(int[] a, int i) {
+        return -1;  // shut up error message
+    }
+
+
+    public static void swap(int [] aos, int i, int j) {
+        int temp = aos[i];
+        aos[i] = aos[j];
+        aos[j] = temp;
+    }
+
+    // Arrays.equals(new int [] {1,2,3,4},
+    //               new int [] {4,3,2,1})
+    public static void reverse(int [] a, int i, int j) {
+
+    }
+
+    // main
+    public static void main(String[] args) {
+
+    }
+
+}  // class Recursion
