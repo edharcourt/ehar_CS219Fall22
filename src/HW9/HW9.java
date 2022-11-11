2d767f934003b1ca2ec33faccca083d60b05273e
edharcourt@gmail.com <edharcourt@gmail.com>
2022-11-11 09:55:05 -0500

    HW 9 again

--- src/HW9/HW9.java
+++ src/HW9/HW9.java
@@ -7,13 +7,16 @@ public class HW9 {
     /*
         Compute x raised to the y power using a simple iterative process
         (i.e., a loop). Do not use BigInteger's built in pow function.
+
+        Use the function pow we wrote in Chap9ImmutableObjects.BigIntegerPlay
+        as a guide.
      */
     public static BigInteger bigpow(BigInteger x, int y) {
         return BigInteger.ZERO;  // shut up error message
     }
 
     /*
-        Compute x raised to the y power using a the recusive definition
+        Compute x raised to the y power using the recursive definition
         from the programming assignment in Canvas. Do not use BigInteger's
         built in pow function.
      */
@@ -25,7 +28,15 @@ public class HW9 {
 
         BigInteger x = new BigInteger("1029883673");
 
-        BigInteger xtoy = fastbigpow(x,10);
+        // Test bigpow
+        BigInteger xtoy = bigpow(x,10);
+        System.out.println(xtoy.toString().equals("1342399346995612672464070989837373830965198185983932347990254056431651542191037112352025649"));
+
+        // make sure our result agrees with BigInteger's built in pow function.
+        System.out.println(bigpow(xtoy, 100).equals(xtoy.pow(100)));
+
+        // Test fastbigpow
+        xtoy = fastbigpow(x,10);
         System.out.println(xtoy.toString().equals("1342399346995612672464070989837373830965198185983932347990254056431651542191037112352025649"));
 
         // make sure our result agrees with BigInteger's built in pow function.
