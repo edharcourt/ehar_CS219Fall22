bf38fedc765726a0aa91a805c5c5fbb85268d445
Ed Harcourt <edharcourt@gmail.com>
2023-02-08 08:32:01 -0500

    Experimenting with interfaces

--- src/HW2/DayOfWeek.java
+++ src/HW2/DayOfWeek.java
@@ -1,5 +1,12 @@
 package HW2;
 
-public class DayOfWeek {
+public class DayOfWeek implements DayOfWeekInterface {
 
+    public int dow(int m, int d, int y ) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
